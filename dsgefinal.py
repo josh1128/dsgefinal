eeeebe754bdd02fdc0aaf64b7d28f3b109c1cbd8
josh1128 <121201154+josh1128@users.noreply.github.com>
2025-08-28 10:33:58 -0600

    Update dsgefinal.py

--- dsgefinal.py
+++ dsgefinal.py
@@ -24,8 +24,7 @@ from pathlib import Path
 import os
 
 # ===== Desktop export folder =====
-file_path = r'C:\Users\AC03537\OneDrive - Alberta Central\Desktop'
-
+file_path = r'C:\Users\AC03537\Desktop'
 # =========================
 # Page setup
 # =========================
